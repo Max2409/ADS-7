95e150301b209b4ad2a1a22489db3e18f5f7966c
Max2409 <maksimmezuev242@gmail.com>
2026-05-30 14:23:48 +0300

    Межуев Максим 25-ИСТ-4-1

--- include/train.h
+++ include/train.h
@@ -3,18 +3,26 @@
 #define INCLUDE_TRAIN_H_
 
 class Train {
- private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+private:
+    struct Car {
+        bool light;
+        Car *next;
+        Car *prev;
+    };
+
+    int operationCount;   // количество перемещений между вагонами
+    Car *startCar;        // точка входа в поезд
+
+public:
+    Train();
+    ~Train();
+
+    Train(const Train&) = delete;
+    Train& operator=(const Train&) = delete;
+
+    void addCar(bool light);
+    int  getLength();
+    int  getOpCount() const;
 };
+
 #endif  // INCLUDE_TRAIN_H_
