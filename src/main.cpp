fccf66a52bb3c7733aa1f5e221c5c6a9d8ee7655
Max2409 <maksimmezuev242@gmail.com>
2026-05-30 14:32:16 +0300

    Межуев Максим 25-ИСТ-4-1

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,43 @@
 // Copyright 2022 NNTU-CS
+// src/main.cpp
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
+    std::cout << "n\tall_off\tall_on\trandom\n";
 
-  while (count--)
-    train.addCar(false);
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
 
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
-  return 0;
+    for (int n = 10; n <= 200; n += 10) {
+        // 1. Все лампочки выключены
+        Train trainOff;
+        for (int i = 0; i < n; ++i) {
+            trainOff.addCar(false);
+        }
+        trainOff.getLength();
+        int opsOff = trainOff.getOpCount();
+
+        // 2. Все лампочки включены
+        Train trainOn;
+        for (int i = 0; i < n; ++i) {
+            trainOn.addCar(true);
+        }
+        trainOn.getLength();
+        int opsOn = trainOn.getOpCount();
+
+        // 3. Случайное состояние
+        Train trainRand;
+        for (int i = 0; i < n; ++i) {
+            trainRand.addCar(std::rand() % 2);
+        }
+        trainRand.getLength();
+        int opsRand = trainRand.getOpCount();
+
+        std::cout << n << "\t" << opsOff << "\t" << opsOn << "\t" << opsRand << "\n";
+    }
+
+    return 0;
 }
